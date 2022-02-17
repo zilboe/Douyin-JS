e86d1e0776820a52955348ec72be5648a30d1b91
zilboe <54744688+zilboe@users.noreply.github.com>
2022-02-17 16:14:49 +0800

    Update 抖音极速版.js

--- 抖音极速版.js
+++ 抖音极速版.js
@@ -8,34 +8,45 @@ function console_启动(){
     sleep(600);
     console.setSize((x1 / 2)-20 , (y / 4)-50);
 }
+
 sleep(random(2000,3000));//启动时间，可以先手动打开，再运行程序
 console_启动();
 console.log("抖音极速版启动成功");
 var i;
 var 视频数量=1;
 var 红包出现=text("恭喜你被红包砸中");
+var 关闭按钮=id("iv_close");
+//用bounds来click
+//var b = desc("打开侧拉菜单").findOne().bounds();
+//click(b.centerX(), b.centerY());
+threads.start(function(){
+    while(true){
+        
+        if(红包出现.exists()){
+        console.info("出现红包");
+        click(x1 / 2 , y / 2);
+        console.info("已经领取");
+        var 寻找关闭按钮=关闭按钮.findOne(100);
+        if(寻找关闭按钮!=null){
+            关闭按钮.click();
+        }
+        else{
+            click(530,1930);
+        }
+        }
+    sleep(600);
+    }
+} )
 while(true)
 {
     for(i=1;i<10;i++)
     {
         var 随机偏移x=random(20,60);
         console.clear();
-        var x=random(5000,6500);
+        var x=random(6000,8500);
         console.log("开始第" + 视频数量 + "个视频");
-        swipe(800,1650,800-随机偏移x,300,350);//无偏移图片过不去
-        if(红包出现.exists()) {
-            console.info("出现红包");
-            click(x1 / 2 , y / 2);
-            console.info("已经领取");
-            var 关闭按钮=id("iv_close").findOne(100);
-            if(关闭按钮!=null){
-                关闭按钮.click();
-            }
-            else{
-                click(530,1930);
-            }
-        }
-        console.log("随机播放" + (x+2500)/1000 + "秒");
+        swipe(800,1650,800-随机偏移x,300,350);
+        console.log("随机播放" + x/1000 + "秒");
         sleep(x);
         视频数量++;
     }
